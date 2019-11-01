c340a3d97330c81867caa6382546fd9ec10a78c5
Masaru Nemoto <16267290+nemolize@users.noreply.github.com>
2019-11-01 22:39:32 +0900

    feat: updates render method

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,35 @@ type User = {
   name: string;
   location?: {
     country: string;
-    city: string;
   };
 };
 
 const users: Array<User> = [
   {
-    name: "name"
+    name: "Nemoto",
+    location: {
+      country: 'Japan'
+    }
+  },
+  {
+    name: "Alex",
+    location: {
+      country: 'United Kingdom'
+    }
+  },
+  {
+    name: "X",
   }
 ];
 
+function renderMessage(){
+  return users.map(
+    user=>`name: ${user.name}, country: ${user.location?.country ?? 'Unknown'}`
+  ).join('\n');
+}
+
 //create a server object:
 createServer(function(req, res) {
-  res.write(`location is ${users[0].location?.country ?? 'not specified'}`); //write a response to the client
+  res.write(renderMessage()); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
